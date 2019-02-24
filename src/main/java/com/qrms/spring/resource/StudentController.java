8b9f93a627dfc7d66a968ec77fd1c01c0a48caf1
bmk15897 <bmk15897@gmail.com>
2019-02-24 13:45:33 +0530

    added elective preference count model

--- src/main/java/com/qrms/spring/resource/StudentController.java
+++ src/main/java/com/qrms/spring/resource/StudentController.java
@@ -16,10 +16,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.qrms.spring.model.Course;
+import com.qrms.spring.model.ElectiveVacancyPrefCounts;
 import com.qrms.spring.model.StudentAcad;
 import com.qrms.spring.model.StudentPref;
 import com.qrms.spring.model.Users;
 import com.qrms.spring.repository.CourseRepository;
+import com.qrms.spring.repository.ElectiveVacancyPrefCountsRepository;
 import com.qrms.spring.repository.StudentAcadRepository;
 import com.qrms.spring.repository.StudentPrefRepository;
 
@@ -36,6 +38,9 @@ public class StudentController {
 	@Autowired
 	private CourseRepository courseRepository;
 
+	@Autowired
+	private ElectiveVacancyPrefCountsRepository electiveVacancyPrefCountsRepository;
+	
 	@GetMapping("/home")
 	public String studentHome() {
 		return "student/home";
@@ -91,15 +96,33 @@ public class StudentController {
 		
 		StudentAcad currUserAcad = studentAcadRepository.findByUserName(userName);
 		
+		
+		Course c1 = courseRepository.findByCourseId(course1);
+		Course c2 = courseRepository.findByCourseId(course2);
+		Course c3 = courseRepository.findByCourseId(course3);
+		Course c4 = courseRepository.findByCourseId(course4);
+		
 		StudentPref studentPref = new StudentPref();
 		studentPref.setUserName(userName);
 		studentPref.setAcademicYear(currUserAcad.getAcademicYear());
-		studentPref.setCourse1(courseRepository.findByCourseId(course1));
-		studentPref.setCourse2(courseRepository.findByCourseId(course2));
-		studentPref.setCourse3(courseRepository.findByCourseId(course3));
-		studentPref.setCourse4(courseRepository.findByCourseId(course4));
+		studentPref.setCourse1(c1);
+		studentPref.setCourse2(c2);
+		studentPref.setCourse3(c3);
+		studentPref.setCourse4(c4);
 
 		studentPrefRepository.save(studentPref);
+		
+		ElectiveVacancyPrefCounts electiveVacancyPrefCounts = electiveVacancyPrefCountsRepository.findByCourseId(course1);
+		
+		electiveVacancyPrefCounts.setCourseId(c1.getCourseId());
+		int prefCount = electiveVacancyPrefCounts.getPrefCount();
+//		int vacancyCount = electiveVacancyPrefCounts.getVacancyCount();
+		
+		electiveVacancyPrefCounts.setPrefCount(++prefCount);
+//		electiveVacancyPrefCounts.setVacancyCount(--vacancyCount);
+		
+		electiveVacancyPrefCountsRepository.save(electiveVacancyPrefCounts);
+		
 		model.addObject("msg","Your preferences for electives have been recorded!");
 		model.setViewName("student/home");
 		return model;
